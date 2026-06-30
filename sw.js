5e188fc87a6e572f887610f608f83a555a7f29ca
Devilmustaf <loranse.ar@gmail.com>
2026-06-30 11:23:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,24 @@
-const CACHE_NAME = 'wallet-v2';
-const ASSETS = ['./', './index.html'];
+const CACHE_NAME = 'wallet-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+// تثبيت الـ Service Worker وحفظ الملفات الأساسية
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS).catch(() => {});
+      return cache.addAll(ASSETS);
     })
   );
 });
 
+// تفعيل واستقبال البيانات (شرط أساسي لظهور زر التثبيت)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
